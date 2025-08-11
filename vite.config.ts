85b5fe00cddd85bdf65619d8c1e453e622dbd047
KhangNguyen <khangng872@gmail.com>
2025-08-11 00:39:38 -0400

    Fix vite Base in config.ts #2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Stars-Research-Corp/StarsResearchCorp.git',
+  base: '/StarsResearchCorp/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
